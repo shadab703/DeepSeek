dd1ba67c83e1a009b689e6016840f1586dfd6cdf
Shadab Siddiqui <shadabsiddiqui547@gmail.com>
2025-08-28 02:22:21 +0530

    deepseek api complete

---
+++ app/api/chat/create/route.js
@@ -0,0 +1,33 @@
+import connectDB from "@/config/db"
+import Chat from "@/models/Chat"
+import { getAuth } from "@clerk/nextjs/server"
+import { NextResponse } from "next/server"
+
+
+export async function POST(req) {
+    try {
+        const { userId } = getAuth(req);
+        if (!user) {
+            return NextResponse.json({ success: false, message: "User not authenticated", })
+        }
+
+        // Prepare the chat data to be saved in the database
+        const chatData = {
+            userId,
+            message: [],
+            name: "New Name",
+        };
+
+        // Connect to the database and create a new Chat
+
+        await connectDB();
+        await Chat.create(chatData);
+        return NextResponse.json({ success: true, message: "Chat is Created" });
+
+
+    } catch (error) {
+        return NextResponse.json({ success: false, error: error.message })
+
+    }
+
+}
